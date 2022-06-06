3fdec2c0feb5ca0024f30c01ad10395cd367ba63
abk <abiekidane19@hotmail.com>
2022-06-06 22:34:40 +0200

    Student csv, and student object

--- src/CSV.java
+++ src/CSV.java
@@ -1,2 +1,39 @@
-public class CSV {
+import java.io.BufferedReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+public final class CSV {
+
+    private CSV() {
+        throw new AssertionError("Cannot instantiate class");
+    }
+
+
+    public static List<Student> readStudent(Path path, String delimiter, Charset cr)  throws Exception {
+        if(path == null || delimiter == null ) {
+            throw new IllegalArgumentException("Path or delimiter is null, cannot read file");
+        }
+        cr = Objects.requireNonNullElse(cr, StandardCharsets.UTF_8);
+
+        List<Student> content = new ArrayList<>();
+        try(BufferedReader br = Files.newBufferedReader(path,cr)) {
+            String line ;
+            while((line = br.readLine()) != null) {
+                String []  values = line.split(Pattern.quote(delimiter));
+                content.add(new Student(values[7],values[8],values[1]));
+
+            }
+        }
+        return content;
+
+    }
+
+
 }
---
+++ src/Student.java
@@ -0,0 +1,45 @@
+public class Student {
+    private String marks;
+    private String hobbies;
+    private String gender;
+
+    public Student(String marks, String hobbies, String gender) {
+        this.marks = marks;
+        this.hobbies = hobbies;
+        this.gender = gender;
+    }
+
+    public String getMarks() {
+        return marks;
+    }
+
+    public void setMarks(String marks) {
+        this.marks = marks;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "marks=" + marks +
+                ", hobbies='" + hobbies + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
+
