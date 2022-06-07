f849186159775ab9c00feadbd7eb03a80bb187e0
abk <abiekidane19@hotmail.com>
2022-06-07 23:14:14 +0200

    Assemby ai response body into json

--- src/main/java/Json/Assembly.java
+++ src/main/java/Json/Assembly.java
@@ -1,15 +1,48 @@
 package Json;
 
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Assembly {
-    private static final String URL = ""
-    public static void main(String[] args) {
-        HttpRequest rewuest = HttpRequest.newBuilder()
-                .uri("https://jsonplaceholder.typicode.com/posts")
+    private static final String URL = "https://api.assemblyai.com/v2/transcript";
+    private static final String audio_url = "https://raw.githubusercontent.com/johnmarty3/JavaAPITutorial/main/Thirsty.mp4";
+    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
+    Transcript trnascript = new Transcript();
+    trnascript.setAudio_url(audio_url);
+        Gson gson = new Gson();
+        String json = gson.toJson(trnascript);
+
+        HttpRequest request  = HttpRequest.newBuilder()
+                .uri(new URI(URL))
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .header("Authorization",CONSTANT.API_KEY)
                 .build();
         HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        trnascript=  gson.fromJson(response.body(),Transcript.class);
+        System.out.println(trnascript.getId());
+        HttpResponse<String> getResponse = null;
+        while(true) {
+            HttpRequest getRequest = HttpRequest.newBuilder()
+                    .uri(new URI(URL + "/" + trnascript.getId()))
+                    .header("Authorization", CONSTANT.API_KEY)
+                    .build();
+             getResponse = client.send(getRequest, HttpResponse.BodyHandlers.ofString());
+            trnascript = gson.fromJson(getResponse.body(), Transcript.class);
+            if (trnascript.getStatus().equals("completed") || trnascript.getStatus().equals("error")) {
+                break;
+            }
+            Thread.sleep(1000);
+            System.out.println("processing");
+        }
+        
 
     }
 }
--- src/main/java/Json/Transcript.java
+++ src/main/java/Json/Transcript.java
@@ -1,4 +1,40 @@
 package Json;
 
 public class Transcript {
+    private String id ;
+    private String text;
+    private String status ;
+    private String audio_url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAudio_url() {
+        return audio_url;
+    }
+
+    public void setAudio_url(String audio_url) {
+        this.audio_url = audio_url;
+    }
 }
